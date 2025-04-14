ba05ed3e8fdb241f701716420540662c9b920178
artemgoryanskiy <artem.goryanskiy@gmail.com>
2025-04-14 21:39:59 +0300

    first commit

---
+++ src/components/base/api.ts
@@ -0,0 +1,45 @@
+export type ApiListResponse<Type> = {
+    total: number,
+    items: Type[]
+};
+
+export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+
+export class Api {
+	readonly baseUrl: string;
+	protected options: RequestInit;
+
+	constructor(baseUrl: string, options: RequestInit = {}) {
+		this.baseUrl = baseUrl;
+		this.options = {
+			headers: {
+				'Content-Type': 'application/json',
+				...((options.headers as object) ?? {}),
+			},
+		};
+	}
+
+	protected async handleResponse(response: Response): Promise<object> {
+		if (response.ok) {
+			return response.json();
+		} else {
+			const data = await response.json();
+			return await Promise.reject(data.error ?? response.statusText);
+		}
+	}
+
+	get(uri: string) {
+		return fetch(this.baseUrl + uri, {
+			...this.options,
+			method: 'GET',
+		}).then(this.handleResponse);
+	}
+
+	post(uri: string, data: object, method: ApiPostMethods = 'POST') {
+		return fetch(this.baseUrl + uri, {
+			...this.options,
+			method,
+			body: JSON.stringify(data),
+		}).then(this.handleResponse);
+	}
+}
---
+++ src/types/index.ts
@@ -0,0 +1,40 @@
+export interface ICard {
+	likes: IUser[];
+	_id: string;
+	name: string;
+	link: string;
+	owner: IUser;
+	createdAt: Date;
+}
+
+export interface IUser {
+	name: string;
+	about: string;
+	avatar: string;
+	_id: string;
+	cohort: string;
+}
+
+export interface ICardsData {
+	cards: ICard[];
+	preview: string | null;
+	addCard(card: ICard): void;
+	deleteCard(cardId: string, payload: Function | null): void;
+	updateCard(card: ICard, payload: Function | null): void;
+	getCard(cardId: string): ICard;
+	checkValidation(data: Record<keyof TCardInfo, string>): boolean;
+}
+
+export interface IUserData {
+	getUserInfo(): TUserPublicInfo;
+	setUserInfo(userData: IUser): void;
+	checkUserValidation(data: Record<keyof TUserPublicInfo, string>): boolean;
+}
+
+export type TCardInfo = Pick<ICard, 'name' | 'link'>;
+
+export type TUserPublicInfo = Pick<IUser, 'name' | 'about' | 'avatar'>;
+
+export type TUserBaseInfo = Pick<IUser, 'name' | 'about'>;
+
+export type TUserAvatar = Pick<IUser, 'avatar'>;
